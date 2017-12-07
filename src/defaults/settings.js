b6385e8f7d4b37526469dcaaf860fb85f21777b8
samussiah <samussiah@gmail.com>
2017-12-07 10:29:48 -0500

    rebuild

--- src/defaults/settings.js
+++ src/defaults/settings.js
@@ -1,15 +1,8 @@
 export default {
 
-<<<<<<< HEAD
-    event_col: 'DOMAIN',
-    event_types: null,
-    event_highlighted: null,
-    event_highlight_color: 'black',
-=======
     /**-------------------------------------------------------------------------------------------\
       Renderer-specific settings
     \-------------------------------------------------------------------------------------------**/
->>>>>>> date-reference-lines
 
       //ID settings
         id_col: 'USUBJID',
@@ -20,21 +13,10 @@ export default {
         event_col: 'DOMAIN',
         event_types: null,
         event_highlighted: null,
+        event_highlight_color: 'black',
 
-<<<<<<< HEAD
-    time_scale: 'Study Day',
-    stdy_col: 'STDY',
-    endy_col: 'ENDY',
-    study_day_range: null,
-    stdt_col: 'STDT',
-    endt_col: 'ENDT',
-    date_range: null,
-    date_format: '%Y-%m-%d',
-    date_display_format: '%b %y', // sync in syncSettings()
-=======
       //Filter settings
         filters: null,
->>>>>>> date-reference-lines
 
       //Grouping settings
         groupings: null,
@@ -44,63 +26,6 @@ export default {
       //Timing settings
         time_scale: 'Study Day',
 
-<<<<<<< HEAD
-    //Standard webcharts settings
-    x: {
-        type: null, // set in syncSettings()
-        column: 'wc_value',
-        label: null, // set in syncSettings()
-        format: null // set in syncSettings()
-    },
-    y: {
-        type: 'ordinal', // set in syncSettings()
-        column: null,
-        label: null,
-        sort: 'earliest',
-        behavior: 'flex',
-        grouping: null
-    },
-    marks: [
-        {
-            type: 'line',
-            per: null, // set in syncSettings()
-            tooltip: null, // set in syncSettings()
-            attributes: {
-                'stroke-width': 4
-            }
-        },
-        {
-            type: 'circle',
-            per: null, // set in syncSettings()
-            tooltip: null, // set in syncSettings()
-            radius: 4,
-            attributes: {
-                'stroke-width': 2
-            }
-        }
-    ],
-    colors: [
-        '#1b9e77',
-        '#d95f02',
-        '#7570b3',
-        '#a6cee3',
-        '#1f78b4',
-        '#b2df8a',
-        '#66c2a5',
-        '#fc8d62',
-        '#8da0cb'
-    ],
-    color_dom: null, // set in syncSettings()
-    legend: {
-        location: 'top',
-        label: 'Event Type',
-        order: null, // set in syncSettings()
-        mark: 'circle'
-    },
-    range_band: 30,
-    margin: { top: 50 }, // for second x-axis
-    resizable: false // can't be resizable so the multiples aren't overlapped by their titles
-=======
           //Study day settings
             stdy_col: 'STDY',
             endy_col: 'ENDY',
@@ -111,6 +36,7 @@ export default {
             endt_col: 'ENDT',
             date_range: null,
             date_format: '%Y-%m-%d',
+            date_display_format: '%b %y', // sync in syncSettings()
 
       //Miscellaneous settings
         seq_col: 'SEQ',
@@ -147,21 +73,15 @@ export default {
                 per: null, // set in syncSettings()
                 tooltip: null, // set in syncSettings()
                 attributes: {
-                    'clip-path': 'url(#1)',
                     'stroke-width': 4,
-                    'stroke-opacity': 1
                 }
             },
             {
                 type: 'circle',
                 per: null, // set in syncSettings()
                 tooltip: null, // set in syncSettings()
-                radius: '3',
+                radius: 4,
                 attributes: {
-                    'clip-path': 'url(#1)',
-                    fill: 'white',
-                    'fill-opacity': 1,
-                    'stroke-opacity': 1,
                     'stroke-width': 2
                 }
             }
@@ -185,8 +105,7 @@ export default {
             mark: 'circle'
         },
         gridlines: 'y',
-        range_band: 24,
+        range_band: 30,
         margin: { top: 50 }, // for second x-axis
         resizable: false // can't be resizable so the multiples aren't overlapped by their titles
->>>>>>> date-reference-lines
 };

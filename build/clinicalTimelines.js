ca6d6390f60a79ca58dcabe19cd7b6e28f503c95
samussiah <samussiah@gmail.com>
2018-03-12 18:16:52 -0400

    move y-axis tick tooltip code to its own block

--- build/clinicalTimelines.js
+++ build/clinicalTimelines.js
@@ -3202,17 +3202,18 @@
         this.svg
             .selectAll('.y.axis .tick')
             .each(function(d) {
-                var tooltip = context.config.id_tooltip_col
-                    ? context.initial_data.find(function(di) {
-                          return di[context.config.id_col] === d;
-                      })[context.config.id_tooltip_col]
-                    : context.config.id_unitPropCased + ': ' + d;
                 if (/^-g\d+-/.test(d)) d3.select(this).remove();
-                else
+                else {
+                    var tooltip = context.config.id_tooltip_col
+                        ? context.initial_data.find(function(di) {
+                              return di[context.config.id_col] === d;
+                          })[context.config.id_tooltip_col]
+                        : context.config.id_unitPropCased + ': ' + d;
                     d3
                         .select(this)
                         .append('title')
                         .text(tooltip);
+                }
             })
             .on('click', function(d) {
                 _this.selected_id = d;
--- src/timelines/callbacks/onResize/tickClick.js
+++ src/timelines/callbacks/onResize/tickClick.js
@@ -9,16 +9,17 @@ export default function tickClick() {
     this.svg
         .selectAll('.y.axis .tick')
         .each(function(d) {
-            const tooltip = context.config.id_tooltip_col
-                ? context.initial_data.find(di => di[context.config.id_col] === d)[
-                      context.config.id_tooltip_col
-                  ]
-                : `${context.config.id_unitPropCased}: ${d}`;
             if (/^-g\d+-/.test(d)) select(this).remove();
-            else
+            else {
+                const tooltip = context.config.id_tooltip_col
+                    ? context.initial_data.find(di => di[context.config.id_col] === d)[
+                          context.config.id_tooltip_col
+                      ]
+                    : `${context.config.id_unitPropCased}: ${d}`;
                 select(this)
                     .append('title')
                     .text(tooltip);
+            }
         })
         .on('click', d => {
             this.selected_id = d;

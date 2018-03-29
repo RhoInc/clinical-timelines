3f7206f0de5d088225cdea93b4f09485b38640e5
samussiah <samussiah@gmail.com>
2018-03-29 11:17:46 -0400

    rebuild

--- src/timelines/callbacks/onLayout/IDdetails.js
+++ src/timelines/callbacks/onLayout/IDdetails.js
@@ -1,6 +1,7 @@
 export default function IDdetails() {
     //Add ID characteristics.
-    this.clinicalTimelines.containers.IDdetails.selectAll('div.characteristic')
+    this.clinicalTimelines.containers.IDdetails
+        .selectAll('div.characteristic')
         .data(this.config.id_characteristics)
         .enter()
         .append('div')
